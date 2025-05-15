410ee26af0edfcfc860ca53022080a8c7db704f4
CodewaveSandy <sandy@codewave.com>
2025-05-16 00:11:17 +0530

    Adding deployment configs for render

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
-import express, {
-  Request,
-  Response,
-  NextFunction,
-  Router,
-  Application,
-} from "express";
+import "dotenv/config";
+import express, { Application } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes";
-import { responseMiddleware } from "./middleware/response.middleware";
+import { successResponse } from "./utils/response.utils";
 
 const app: Application = express();
 
@@ -17,10 +13,14 @@ const app: Application = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(responseMiddleware);
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 // Routes
+app.use("/api/health", (_req, res) => {
+  successResponse(res, null, "API is healthy");
+  return;
+});
 app.use("/api/users", userRoutes);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import "dotenv/config";
-import express from "express";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-import userRoutes from "./routes/user.routes";
 import { connectDB } from "./config/database";
-
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-
-app.use("/api/users", userRoutes);
+import app from "./app";
+import { logger } from "./config/logger";
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,7 +8,7 @@ const startServer = async () => {
   await connectDB();
 
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
   });
 };
 
